b290f7220a33eadc4b22335375e53421efef6879
Funix565 <denkin.dmitriy@gmail.com>
2023-10-20 00:04:14 +0300

    feat: implemented "Ping" action

---
+++ BridgeDogs/Controllers/PingController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BridgeDogs.Controllers
+{
+    [Route("ping")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        private const string VERSION = "Dogshouseservice.Version1.0.1";
+
+        [HttpGet]
+        public string Ping()
+        {
+            // TODO: I guess we can use `Assembly` and Reflection here to avoid hardcoded string.
+            return VERSION;
+        }
+    }
+}
