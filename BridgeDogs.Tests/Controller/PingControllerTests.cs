7679a654e605fdb9d675749f4c20b595e4570569
Funix565 <denkin.dmitriy@gmail.com>
2023-10-23 01:21:58 +0300

    test: write test for PingController

---
+++ BridgeDogs.Tests/Controller/PingControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using BridgeDogs.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeDogs.Tests.Controller
+{
+    public class PingControllerTests
+    {
+        [Fact]
+        public void Ping_ReturnsVersion()
+        {
+            // Arrange
+            var controller = new PingController();
+
+            // Act
+            var result = controller.Ping();
+
+            // Assert
+            Assert.Equal("Dogshouseservice.Version1.0.1", result.Content);
+        }
+    }
+}
